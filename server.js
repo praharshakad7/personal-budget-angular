440b8e63baa3910ddad852217c77d1e84fc535b5
K.PraharshaSrikar <kadamaripraharshasrikar@gmail.com>
2023-09-23 02:23:35 -0400

    angular commit

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const fs = require('fs');
 
+app.use(cors());
+const budget = {
+    "myBudget": [
+        {
+            "title": "Eat out",
+            "budget": 25
+        },
+        {
+            "title": "Rent",
+            "budget": 275
+        },
+        {
+            "title": "Grocery",
+            "budget": 110
+        },
+        {
+            "title": "Transportation",
+            "budget": 60
+        },
+        {
+            "title": "Entertainment",
+            "budget": 50
+        },
+        {
+            "title": "Utilities",
+            "budget": 120
+        },
+        {
+            "title": "Insurance",
+            "budget": 100
+        }
+    ]
+}
+
+
 // Serve static files
-app.use(express.static(__dirname));
-app.use('/', express.static('public'));
+//app.use(express.static(__dirname));
+//app.use('/', express.static('public'));
 // Endpoint to serve budget.json
+
 app.get('/budget', (req, res) => {
     fs.readFile('budget.json', 'utf8', (err, data) => {
         if (err) {
@@ -18,5 +55,5 @@ app.get('/budget', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
